67f678e70f07c82a9a84cc2cf795e4f0cd40861e
Franco Barpp Gomes <franco.b.gomes@gmail.com>
2019-04-24 19:05:10 -0300

    Exercício 1

--- projeto1/ex1/ex1.c
+++ projeto1/ex1/ex1.c
@@ -1,25 +1,203 @@
 
-// =====================================
+// ==================================================
 //  ex1.c
 //
 //  Autores: Franco Barpp Gomes - 2126613
 //           Patricia Abe Turato - <TODO: COLOCAR MATRICULA>
 //
 //  Projeto Prático I
-// =====================================
+// ==================================================
 
 #include <stdio.h>
+#include <string.h>
 
-void imprimirCabecalho(int mes);
-void imprimirMes(int mes);
-int diaDaSemana(int mes, int ano);
+void imprimirCabecalho(int mes, int ano);
+
+int imprimirConteudo(int mes, int ano, int diaSemana);
+
+int imprimirMes(int mes, int ano, int diaSemana);
+
+void imprimirCalendario(int ano);
+
 int isBissexto(int ano);
-void imprimirConteudo(int mes);
-int diasMes(int mes);
 
-int main()
-{
+int diasMes(int mes, int ano);
 
+int primeiroDiaAno(int ano);
+
+void centralizarCabecalho(int mes);
+
+void printEspacos(int n);
+
+int main(){
+    int ano;
+    
+    printf("Insira o ano do calendário: ");
+    scanf("%d", &ano);
+    
+    imprimirCalendario(ano);
+    
     return 0;
+}
 
+void imprimirCabecalho(int mes, int ano) {
+
+    printf("\n\n");
+    centralizarCabecalho(mes); // centraliza o cabeçalho superior
+    
+    switch(mes) {
+        case 0:
+            printf("Janeiro de %d", ano);
+            break;
+        case 1:
+            printf("Fevereiro de %d", ano);
+            break;
+        case 2:
+            printf("Marco de %d", ano);
+            break;
+        case 3:
+            printf("Abril de %d", ano);
+            break;
+        case 4:
+            printf("Maio de %d", ano);
+            break;
+        case 5:
+            printf("Junho de %d", ano);
+            break;
+        case 6:
+            printf("Julho de %d", ano);
+            break;
+        case 7:
+            printf("Agosto de %d", ano);
+            break;
+        case 8:
+            printf("Setembro de %d", ano);
+            break;
+        case 9:
+            printf("Outubro de %d", ano);
+            break;
+        case 10:
+            printf("Novembro de %d", ano);
+            break;
+        case 11:
+            printf("Dezembro de %d", ano);
+            break;
+    }
+
+    printf("\nDOM SEG TER QUA QUI SEX SAB\n");
+
+}
+
+int imprimirConteudo(int mes, int ano, int diaSemana) {
+
+    int nDias = diasMes(mes, ano);
+
+    printEspacos(3*diaSemana); // enche de espaços onde seriam os dias do outro mês
+
+    for(int i=1; i<=nDias; i++) {
+        diaSemana = diaSemana%7;
+
+        if(diaSemana == 0 && i>1) printf("\n");
+
+        printf("%3d ", i);
+
+        diaSemana++;
+    }
+
+    return diaSemana%7;
+
+}
+
+int imprimirMes(int mes, int ano, int diaSemana) {
+
+    imprimirCabecalho(mes, ano);
+    int diaSemanaProx = imprimirConteudo(mes, ano, diaSemana);
+
+    return diaSemanaProx;
+
+}
+
+int isBissexto(int ano){
+    return ano%4==0 && (ano%100!=0 || ano%400==0);
+}
+
+int diasMes(int mes, int ano){
+
+    if(mes == 1) {
+        if(isBissexto(ano)) return 29;
+        return 28;
+    }
+
+    else if(mes <= 6){
+        if(mes % 2 == 0) return 31;
+        return 30;
+    }
+
+    else{
+        if(mes % 2 == 0) return 30;
+        return 31;
+    }
+
+}
+
+int primeiroDiaAno(int ano) {
+
+    // TODO: explicar congruencia de zeller e explicar pq +1 da certo
+
+    int k = ano % 100;
+    int j = ano / 100;
+
+    int diaSemana = 1 + 52/10 + k + k/4 + j/4 + 5*j;
+    diaSemana = diaSemana % 7;
+
+    return diaSemana+1;
+
+}
+
+void imprimirCalendario(int ano) {
+
+    int primeiroDia = primeiroDiaAno(ano);
+
+    for(int i=0; i<12; i++){
+        primeiroDia = imprimirMes(i, ano, primeiroDia); // atualiza o primeiro dia da semana de cada mês sem necessidade de usar a congruencia de zeller dnv
+    }
+
+}
+
+void centralizarCabecalho(int mes) {
+    
+    int nEspacos;
+    int tamanhoMes;
+    int tamanhoLinha = strlen("DOM SEG TER QUA QUI SEX SAB"); // pega o tamanho total da linha
+    
+    // pega o tamanho do nome do mês
+    switch(mes){
+       case 0: tamanhoMes = strlen("Janeiro"); break;
+       case 1: tamanhoMes = strlen("Fevereiro"); break;
+       case 2: tamanhoMes = strlen("Marco"); break;
+       case 3: tamanhoMes = strlen("Abril"); break;
+       case 4: tamanhoMes = strlen("Maio"); break;
+       case 5: tamanhoMes = strlen("Junho"); break;
+       case 6: tamanhoMes = strlen("Julho"); break;
+       case 7: tamanhoMes = strlen("Agosto"); break;
+       case 8: tamanhoMes = strlen("Setembro"); break;
+       case 9: tamanhoMes = strlen("Outubro"); break;
+       case 10: tamanhoMes = strlen("Novembro"); break;
+       case 11: tamanhoMes = strlen("Dezembro"); break;
+    }
+    
+    // pra que o texto esteja centralizado, o tamanho dele (contando os espaços) deve ser igual ao tamanho da linha
+    // logo, o numero de espaços deve ser igual ao tamanho livre (tamanhoLinha - tamanhoEscrito) dividido por dois (tem dois lados)
+    // como só precisa printar de um lado, printa só o número de espaços antes
+    
+    nEspacos = (tamanhoLinha - (tamanhoMes + strlen(" de AAAA")))/2; 
+    
+    printEspacos(nEspacos);
+    
+}
+
+void printEspacos(int n) {
+    for(int i=0;i<n;i++){
+        putchar(' ');
+    }
 }
